be1b1dae97667f6bb51c1c53f744e95537c4df2b
ligao8 <ligao8@iflytek.com>
2019-02-01 10:13:19 +0800

    reset project

--- src/main/java/com/iflytek/mapper/MessageMapper.java
+++
@@ -1,7 +0,0 @@
-package com.iflytek.mapper;
-
-import com.iflytek.pojo.Message;
-import com.iflytek.utils.MyMapper;
-
-public interface MessageMapper extends MyMapper<Message> {
-}
--- src/main/java/com/iflytek/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.iflytek.mapper;
-
-import com.iflytek.pojo.User;
-import com.iflytek.utils.MyMapper;
-
-public interface UserMapper extends MyMapper<User> {
-}
